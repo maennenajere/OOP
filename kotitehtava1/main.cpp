522720b76e33ea0c21595e1fdf62bf9ff17e3c60
Jere <jere.maennena@gmail.com>
2024-01-12 17:12:09 +0200

    changes

---
+++ kotitehtava1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int satluku = rand() % 40;
+    cout << satluku << endl;
+    satluku = rand() % 40;
+    cout << satluku << endl;
+    satluku = rand() % 40;
+    cout << satluku << endl;
+    satluku = rand() % 40;
+    cout << satluku << endl;
+    satluku = rand() % 40;
+    cout << satluku << endl;
+}
--- teht0/main.cpp
+++ teht0/main.cpp
@@ -1,9 +1,42 @@
+/*exercise during the lecture*/
+
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World TVT23SPO!" << endl;
-    return 0;
+float keskiarvo(void) {
+    float taulukko[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
+    float ka = 0.0;
+
+    for (int i = 0; i < 5; i++) {
+        ka = ka + taulukko[i];
+    }
+    ka = ka / 5.0;
+    return ka;
 }
+
+int main()
+{   /*
+        int kokonaisluku = 32;
+        float liukuluku1 = 3.145;
+        double liukuluku2 = 56.56;
+        char a = 'b';
+        string merkkijono = "yykaanee";
+
+        int luku = 0;
+        cout << "Anna luku? " << endl;
+        cin >> luku;
+        cout << "Annoit luvun " << luku << endl;
+    */
+
+    /*
+        cout << "Moro maailma TVT23SPO!:)" << endl;
+        cout << merkkijono << endl;
+    */
+
+        float tulos = keskiarvo();
+    cout << setprecision(5) << tulos << endl;
+        return 0;
+}
+
