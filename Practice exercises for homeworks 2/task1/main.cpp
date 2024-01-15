a69c9e04d15bf34759f6620eee03d305221de342
Jere <jere.maennena@gmail.com>
2024-01-16 00:37:34 +0200

    Add new practice exercises for homeworks 2

---
+++ Practice exercises for homeworks 2/task1/main.cpp
@@ -0,0 +1,26 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+
+class myClass
+{
+public:
+
+    myClass();
+
+
+    ~myClass();
+
+private:
+
+    string privateTextToPrint;
+
+    void privateFunction();
+
+protected:
+
+
+};
+
+
+#endif // MYCLASS_H
