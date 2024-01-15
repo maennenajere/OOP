888bf538f872742c9f068e96e092205020c90e4a
Jere <jere.maennena@gmail.com>
2024-01-16 01:02:19 +0200

    initial commit

---
+++ Practice exercises for homeworks 2/task1/main.cpp
@@ -0,0 +1,24 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+class myClass
+{
+
+public:
+
+    myClass();
+
+    ~myClass();
+
+private:
+
+    string privateTextToPrint;
+
+    void privateFunction();
+
+protected:
+};
+
+#endif // MYCLASS_H
+
+//coming soon...
