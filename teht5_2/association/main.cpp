ed1f55cd4bc1da9911d5aa2e9db2fecee14ee117
Jere Mäennenä <jere.maennena@gmail.com>
2024-02-08 00:51:28 +0200

    initial commit

---
+++ teht5_2/association/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "association coming soon!" << endl;
+    return 0;
+}
---
+++ teht5_2/inheritance/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "inheritance coming soon!" << endl;
+    return 0;
+}
